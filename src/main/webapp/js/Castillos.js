9a382d66f136a9b0fb1c40c44400df732b355c44
Thomas Wilczak <tom.wilczak@gmail.com>
2011-09-06 20:16:15 -0500

    added basic script using Three.js

--- src/main/webapp/js/Castillos.js
+++ src/main/webapp/js/Castillos.js
@@ -1,8 +1,35 @@
 function Castillos(){};
 
+var camera, scene, renderer, geometry, material, mesh;
+
 Castillos.init = function(){
-	var canvas = document.getElementById("canvas");
-	var context = canvas.getContext("2d");
-	
-	context.fillRect(10, 10, 50, 50);
+	camera = new THREE.Camera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+	camera.position.z = 1000;
+
+    scene = new THREE.Scene();
+    
+    geometry = new THREE.CubeGeometry( 200, 200, 200 );
+    material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+    
+    mesh = new THREE.Mesh( geometry, material );
+    
+    scene.addObject( mesh );
+
+    renderer = new THREE.CanvasRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    document.body.appendChild( renderer.domElement );
+    Castillos.animate();
+};
+
+Castillos.animate = function(){
+    requestAnimationFrame( this );
+    Castillos.render();
+};
+
+Castillos.render = function(){
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
+
+    renderer.render( scene, camera );
 };
