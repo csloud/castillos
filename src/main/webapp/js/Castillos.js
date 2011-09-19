668e7a46d1e6815ef7e8c60c758bdb49ecdee49f
Thomas Wilczak <tom.wilczak@gmail.com>
2011-09-18 20:30:34 -0500

    Updated Castillos to draw base grid. Added PanController

--- src/main/webapp/js/Castillos.js
+++ src/main/webapp/js/Castillos.js
@@ -4,13 +4,18 @@ var Castillos = function(){
 	}
 };
 
-var THREE, window, requestAnimationFrame, 
-	camera, scene, renderer, geometry, material, mesh;
+var THREE, window, requestAnimationFrame, PanController,
+	camera, scene, renderer, geometry, material, mesh, projector;
 
 Castillos.prototype.init = function(){
+
+	PanController = new PanController(window);
+	
 	camera = new THREE.Camera( 50, window.innerWidth / window.innerHeight, 1, 10000 );
 	camera.position.z = 1000;
 
+	projector = new THREE.Projector();
+	
     scene = new THREE.Scene();
     
     geometry = new THREE.CubeGeometry( 400, 200, 200 );
@@ -20,21 +25,47 @@ Castillos.prototype.init = function(){
     mesh = new THREE.Mesh( geometry, material );
     mesh.rotation.x = 0.50;
     mesh.rotation.y = 0.65;
-    
-    cylinder = new THREE.CylinderGeometry( 20, 100, 100, 100 );
-    cylMesh = new THREE.Mesh( cylinder, material );
-    cylMesh.rotation.x = 2.05;
-    cylMesh.rotation.y = 0.0;
+    mesh.position.x = 100;
     
     scene.addObject( mesh );
-    scene.addObject(cylMesh);
 
+    this.initGrid();
+    
     renderer = new THREE.CanvasRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
 
+    //TODO: can we have the renderer work with an existing element? 
     document.body.appendChild( renderer.domElement );
-
+    
     renderer.render( scene, camera );
+};
+
+Castillos.prototype.initGrid = function() {
+	
+	var geometry, linesMaterial, plane, i, line;
+	
+	geometry = new THREE.Geometry();
+	geometry.vertices.push( new THREE.Vertex( new THREE.Vector3( - 500, 0, 0 ) ) );
+	geometry.vertices.push( new THREE.Vertex( new THREE.Vector3( 500, 0, 0 ) ) );
+	
+	linesMaterial = new THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.2 } );
+
+	for ( i = 0; i <= 20; i ++ ) {
+
+		line = new THREE.Line( geometry, linesMaterial );
+		line.position.z = ( i * 50 ) - 500;
+		scene.addObject( line );
+
+		line = new THREE.Line( geometry, linesMaterial );
+		line.position.x = ( i * 50 ) - 500;
+		line.rotation.y = 90 * Math.PI / 180;
+		scene.addObject( line );
+
+	}
+	
+	plane = new THREE.Mesh( new THREE.PlaneGeometry( 1000, 1000 ), linesMaterial );
+	plane.rotation.x = 0.5;
+	scene.addObject( plane );
 };
 
 
